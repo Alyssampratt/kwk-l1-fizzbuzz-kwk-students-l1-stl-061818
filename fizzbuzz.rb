a5173c399eae73040ad0d3e46f32a35bffcd1900
Alyssampratt <magoopratt@gmail.com>
2018-06-19 18:12:22 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,8 @@
 #if a number is divisible by 3, we want to return "fizz"
-#if a number is divisible by 5, return buzz
+#if a number is divisible by 5, return "buzz"
+#if a number is divisible by 3 &5 return "fizz buzz"
+
+def fizzbuzz(x)
+  puts "you gave me #{x}"
+end
+fizzbuzz(3)
